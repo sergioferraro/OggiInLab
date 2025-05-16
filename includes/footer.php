1caf327dbc3a49f874c11fd617df093c1561db9b
sergioferraro <finalbout.geo@gmail.com>
2025-05-16 21:35:44 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
     <div class="container text-center small text-muted">
         &copy; <?php echo date("Y"); ?> OggiInLab | Sergio Ferraro <br>
         Rilasciato sotto licenza <a href="LICENSE" target="_blank">MIT</a> |
-        <a href="https://github.com/nzthunter/oggi-inlab" target="_blank">GitHub</a>
+        <a href="https://github.com/sergioferraro/OggiInLab" target="_blank">GitHub</a>
         <br>
         <a href="privacy.php">Informativa privacy</a>
 </footer>
