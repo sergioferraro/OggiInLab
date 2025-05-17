eba2f1474c7aa489eb7135e085e047e6e9ce2aa9
Sergio Ferraro <finalbout.geo@gmail.com>
2025-05-17 08:28:05 +0200

    Add files via upload

---
+++ privacy.example.php
@@ -0,0 +1,58 @@
+<?php
+// privacy.php
+/*
+ * OggiInLab
+ * Copyright (c) 2025 Sergio Ferraro
+ * Licensed under the MIT License
+ */
+?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Privacy & Cookie Policy - OggiInLab</title>
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <!-- BOOTSTRAP CORE STYLE (CYBORG) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/cyborg/bootstrap.min.css" rel="stylesheet">
+
+    <!-- FONT AWESOME STYLE  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <?php include "includes/header.php"; ?>
+    <div class="container mt-5">
+        <h1>Privacy & Cookie Policy</h1>
+
+        <h2>Privacy Policy</h2>
+        <p>
+            Privacy Policy
+        </p>
+
+
+        <h2>Cookie Policy</h2>
+        <p>
+            Cookie Policy
+        </p>
+        
+
+        <h2>Contatti</h2>
+        <p>
+            Contatto
+        </p>
+        
+
+        <p class="mt-4">
+            Torna alla <a href="dashboard.php">pagina principale</a>.
+        </p>
+    </div>
+
+    <?php
+    include('includes/footer.php');
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+</body>
+</html>
